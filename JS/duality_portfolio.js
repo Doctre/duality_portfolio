59a593b4d5acaed5a1f5ca819caf0649a0ec0b9e
ActualSaitama <105046440+Doctre@users.noreply.github.com>
2023-09-09 15:14:15 +0300

    Update duality_portfolio.js

--- JS/duality_portfolio.js
+++ JS/duality_portfolio.js
@@ -1,30 +1,31 @@
 document.addEventListener("DOMContentLoaded", function () {
+  
+    // Video player functionality
     const gallery = document.querySelector(".gallery");
-
-    function createVideoPlayers() {
-        gallery.querySelectorAll(".image-wrapper").forEach((imageWrapper) => {
-            const videoUrl = imageWrapper.dataset.videoId;
-            imageWrapper.addEventListener("click", () => showVideoPlayer(videoUrl));
-        });
+  
+    function createVideoPlayer(videoUrl) {
+      const videoPlayer = document.createElement("div");
+      videoPlayer.classList.add("video-player");
+  
+      const iframe = document.createElement("iframe");
+      iframe.src = "https://www.youtube.com/embed/" + videoUrl;
+      iframe.allowFullscreen = true;
+      iframe.style.width = "80%"; // Set the width to 80% of the container
+  
+      videoPlayer.appendChild(iframe);
+  
+      // Remove any existing video players before adding the new one
+      const existingVideoPlayers = gallery.querySelectorAll(".video-player");
+      existingVideoPlayers.forEach((player) => player.remove());
+  
+      gallery.appendChild(videoPlayer);
     }
-
-    function showVideoPlayer(videoUrl) {
-        const videoPlayer = document.createElement("div");
-        videoPlayer.classList.add("video-player");
-
-        const iframe = document.createElement("iframe");
-        iframe.src = "https://www.youtube.com/embed/" + videoUrl;
-        iframe.allowFullscreen = true;
-        iframe.style.width = "80%"; // Set the width to 80% of the container
-
-        videoPlayer.appendChild(iframe);
-
-        // Remove any existing video players before adding the new one
-        const existingVideoPlayers = document.querySelectorAll(".video-player");
-        existingVideoPlayers.forEach((player) => player.remove());
-
-        gallery.appendChild(videoPlayer);
+  
+    function attachVideoPlayerClickEvent(imageWrapper) {
+      const videoUrl = imageWrapper.dataset.videoId;
+      imageWrapper.addEventListener("click", () => createVideoPlayer(videoUrl));
     }
-
-    createVideoPlayers();
-});
+  
+    gallery.querySelectorAll(".image-wrapper").forEach(attachVideoPlayerClickEvent);
+  });
+  
